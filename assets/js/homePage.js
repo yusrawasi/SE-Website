af3f1c1f8877c456f149ab3890f637f667011a55
Weeknd <naqvi_hasan@live.com>
2017-12-29 21:25:02 +0500

    first Commit
    Header and parts of the slider.

---
+++ assets/js/homePage.js
@@ -0,0 +1,54 @@
+//COPIED. Don't sweat
+// (function($) { // Begin jQuery
+//   $(function() { // DOM ready
+//     // If a link has a dropdown, add sub menu toggle.
+//     $('nav ul li a:not(:only-child)').click(function(e) {
+//       $(this).siblings('.nav-dropdown').toggle();
+//       // Close one dropdown when selecting another
+//       $('.nav-dropdown').not($(this).siblings()).hide();
+//       e.stopPropagation();
+//     });
+//     // Clicking away from dropdown will remove the dropdown class
+//     $('html').click(function() {
+//       $('.nav-dropdown').hide();
+//     });
+//     // Toggle open and close nav styles on click
+//     $('#nav-toggle').click(function() {
+//       $('nav ul').slideToggle();
+//     });
+//     // Hamburger to X toggle
+//     $('#nav-toggle').on('click', function() {
+//       this.classList.toggle('active');
+//     });
+//   }); // end DOM ready
+// })(jQuery); // end jQuery
+
+
+// CODE FOR THE SLIDESHOW
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides"); //Get all slides present.
+    for (i = 0; i < slides.length; i++) { //Loop through and turn the display off.
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++; //One image has to be displayed
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; //Turn this images display to 'block' for display
+    setTimeout(showSlides, 1500); // Change image every 1.5 seconds *Change this number if too frequent*
+}
