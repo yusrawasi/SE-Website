bebfbc0a9552372de9f9083f33cde280a0551b46
Usman Sabir <usmansabir98@hotmail.com>
2017-12-30 23:58:33 +0500

    add carousel news slider

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -33,11 +33,17 @@ $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
  
-      autoPlay: 3000, //Set AutoPlay to 3 seconds
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:false
  
-      items : 4,
-      itemsDesktop : [1199,3],
-      itemsDesktopSmall : [979,3]
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
  
   });
  
