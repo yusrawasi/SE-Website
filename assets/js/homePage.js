6c5a70d82ee1619f48f41b3aaa7decdc9230657f
Usman Sabir <usmansabir98@hotmail.com>
2017-12-30 19:24:05 +0500

    add parallax and content

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -28,3 +28,17 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block"; //Turn this images display to 'block' for display
     setTimeout(showSlides, 1500); // Change image every 1.5 seconds *Change this number if too frequent*
 }
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
