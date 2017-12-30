a08746cd5c1cb76559a51cd9a16e843aa7bfa96c
Yusra Wasi <wasiyusra@gmail.com>
2017-12-30 20:38:30 +0500

    3rd commit

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -42,3 +42,14 @@ $(document).ready(function() {
   });
  
 });
+var wel=document.getElementById("chair");
+
+    wel.addEventListener("mouseover", function(){
+        wel.textContent = "Message from the Chairman!";
+
+     });
+    wel.addEventListener("mouseout", function(){
+        wel.textContent = " ";
+
+     });
+
