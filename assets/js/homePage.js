5edf60b9c70a82215df17c7590a80e3e5e2d6f9b
Weeknd <naqvi_hasan@live.com>
2017-12-31 00:04:59 +0500

    fixed Navbar and added Carousel
    Both navbar and the carousel are using Bootstrap

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -1,34 +1,5 @@
 
 
-// CODE FOR THE SLIDESHOW
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides"); //Get all slides present.
-    for (i = 0; i < slides.length; i++) { //Loop through and turn the display off.
-        slides[i].style.display = "none"; 
-    }
-    slideIndex++; //One image has to be displayed
-    if (slideIndex > slides.length) {slideIndex = 1} 
-    slides[slideIndex-1].style.display = "block"; //Turn this images display to 'block' for display
-    setTimeout(showSlides, 1500); // Change image every 1.5 seconds *Change this number if too frequent*
-}
-
 $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
