56e84176a7d502bd948af02ed6b6bbee4dbdd7d2
Yusra Wasi <wasiyusra@gmail.com>
2017-12-31 15:32:13 +0500

    Message updated

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -19,14 +19,14 @@ $(document).ready(function() {
   });
  
 });
-var wel=document.getElementById("chair");
+// var wel=document.getElementById("chair");
 
-    wel.addEventListener("mouseover", function(){
-        wel.textContent = "Message from the Chairman!";
+//     wel.addEventListener("mouseover", function(){
+//         wel.textContent = "Message from the Chairman!";
 
-     });
-    wel.addEventListener("mouseout", function(){
-        wel.textContent = " ";
+//      });
+//     wel.addEventListener("mouseout", function(){
+//         wel.textContent = " ";
 
-     });
+//      });
 
